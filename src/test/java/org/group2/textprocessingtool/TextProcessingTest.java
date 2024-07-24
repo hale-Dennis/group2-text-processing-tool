17b515a50f24e7d10b106f65a4b6a72193ed50ef
Eben <arkoharkohaddoebenezer21@gmail.com>
2024-07-24 21:01:05 +0000

    refactor: removed initial files as they are no longer needed

---
+++ src/test/java/org/group2/textprocessingtool/TextProcessingTest.java
@@ -0,0 +1,14 @@
+package org.group2.textprocessingtool;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class TextProcessingTest {
+
+    @Test
+    void exampleTest() {
+        assertEquals(1, 1);
+    }
+}
