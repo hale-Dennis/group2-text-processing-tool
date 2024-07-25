3643f6253fb0e9b2cdc0e586b489496fa6984319
amponsemmichael <amponsem_michael@yahoo.com>
2024-07-25 17:06:48 +0000

    added search functionality

--- src/main/java/org/group2/textprocessingtool/controllers/TextProcessingMainController.java
+++ src/main/java/org/group2/textprocessingtool/controllers/TextProcessingMainController.java
@@ -2,11 +2,14 @@ package org.group2.textprocessingtool.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextInputDialog;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.util.Optional;
 
 public class TextProcessingMainController {
 
@@ -54,6 +57,65 @@ public class TextProcessingMainController {
     }
 
     public void handleFind(ActionEvent actionEvent) {
+        TextInputDialog dialog = new TextInputDialog();
+        dialog.setTitle("Search");
+        dialog.setHeaderText("Enter a word to search:");
+        dialog.setContentText("Word:");
+
+        Optional<String> result = dialog.showAndWait();
+        result.ifPresent(word -> {
+            handleSearch(word);
+            System.out.println("Searching for: " + word);
+        });
+
+    }
+
+    @FXML
+    private void handleSearch(String word) {
+
+        String inputText = textInputArea.getText();
+
+        if(inputText.contains(word)){
+            int occurrence = countOccurrences(inputText, word);
+            showAlert("Word search", word + " occurs " + occurrence + " times in text" );
+        }
+        else{
+            showAlert("Word search", word + " is not in text");
+        }
+
+        //searchItem.setText("");
+    }
+
+    public static int countOccurrences(String text, String word) {
+        if (text == null || word == null || word.isEmpty()) {
+            return 0;
+        }
+
+        String[] words = text.split("\\s+");
+        int count = 0;
+
+        for (String w : words) {
+            if (w.equals(word)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    private void showAlert(String title, String content, String header) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(header);
+        alert.setContentText(content);
+        alert.showAndWait();
+    }
+
+    private void showAlert(String title, String content) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setContentText(content);
+        alert.showAndWait();
     }
 
     public void handleReplace(ActionEvent actionEvent) {
@@ -76,6 +138,5 @@ public class TextProcessingMainController {
 
     public void handleAbout(ActionEvent actionEvent) {
     }
-
 
 }
