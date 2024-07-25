5f865c2a0f263cc07afa5439fd1430289873935b
Dennis Owusu <57267272+hale-Dennis@users.noreply.github.com>
2024-07-25 10:29:37 -0700

    Revert "Feature/support_for_saving_and_loading_from_file"

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,9 @@ module org.group2.textprocessingtool {
     requires javafx.fxml;
 
 
-    opens org.group2.textprocessingtool.controllers to javafx.fxml;
-    exports org.group2.textprocessingtool;
+    opens org.group2.textprocessingtool to javafx.fxml;
+    exports org.group2.textprocessingtool.controllers;
 
+    // Add the following line to export the package to javafx.graphics
+    exports org.group2.textprocessingtool to javafx.graphics;
 }
