ae0518ac51bc71cdefb29ba84c0e2fe33638fc0d
Eben <arkoharkohaddoebenezer21@gmail.com>
2024-07-25 16:36:20 +0000

    feat (SCRUM-13): load files implementation

--- src/main/java/org/group2/textprocessingtool/controllers/TextProcessingMainController.java
+++ src/main/java/org/group2/textprocessingtool/controllers/TextProcessingMainController.java
@@ -4,9 +4,13 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class TextProcessingMainController {
 
@@ -27,6 +31,29 @@ public class TextProcessingMainController {
     }
 
     public void handleOpen(ActionEvent actionEvent) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open File");
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Text Files", "*.txt"));
+        File file = fileChooser.showOpenDialog(primaryStage);
+
+        if (file != null) {
+            getTextFromFile(file);
+            currentFile = file;
+        }
+    }
+    
+     private void getTextFromFile(File file) {
+        StringBuilder content = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                content.append(line).append("\n");
+            }
+        } catch (IOException e) {
+            // Handle the exception
+            e.printStackTrace();
+        }
+        textInputArea.setText(content.toString());
     }
 
     public void handleSaveTwo(ActionEvent actionEvent) {
